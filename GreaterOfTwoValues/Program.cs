b1b1e7a4d76ee21a6ae9bc09a41d75bf7777b2c5
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 13:01:46 +0300

    Add files via upload

---
+++ GreaterOfTwoValues/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace GreaterOfTwoValues
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string type = Console.ReadLine();
+            string firstValue = Console.ReadLine();
+            string secondValue = Console.ReadLine();
+            if (type == "string")
+            {
+                GetMax(firstValue, secondValue);
+            }
+            else if (type == "char")
+            {
+                char a = char.Parse(firstValue);
+                char b = char.Parse(secondValue);
+                GetMax(a, b);
+            }
+            else
+            {
+                GetMax(int.Parse(firstValue), int.Parse(secondValue));
+            }
+        }
+        static void GetMax(int a, int b)
+        {
+            Console.WriteLine(a < b ? b : a);
+        }
+
+        static void GetMax(string a, string b)
+        {
+            int result = a.CompareTo(b);
+            Console.WriteLine(result > 0 ? a : b);
+        }
+        static void GetMax(char a, char b)
+        {
+            Console.WriteLine(a < b ? b : a);
+        }
+    }
+}
---
+++ LongerLine/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace LongerLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+
+            double length1 = CalculateDistance(x1, y1, x2, y2);
+            double length2 = CalculateDistance(x3, y3, x4, y4);
+
+            if (length1 >= length2)
+            {
+                PrintLine(x1, y1, x2, y2);
+            }
+            else
+            {
+                PrintLine(x3, y3, x4, y4);
+            }
+        }
+
+        static void PrintLine(double x1, double y1, double x2, double y2)
+        {
+            if (CalculateDistance(x1, y1) <= CalculateDistance(x2, y2))
+            {
+                Console.WriteLine($"({x1}, {y1})({x2}, {y2})");
+            }
+            else
+            {
+                Console.WriteLine($"({x2}, {y2})({x1}, {y1})");
+            }
+        }
+
+        static double CalculateDistance(double x, double y)
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        static double CalculateDistance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        }
+    }
+}
