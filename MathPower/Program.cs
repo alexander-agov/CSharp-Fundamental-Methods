9df2fc4397d98a0d9235905a67ef33f74c821f14
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 13:07:15 +0300

    Add files via upload

---
+++ MathPower/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MathPower
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = MathPower(number, power);
+            Console.WriteLine(result);
+        }
+        static double MathPower(double b, int p)
+        {
+            double r = 1;
+            for (int i = 0; i < p; i++)
+                r *= b;
+            return r;
+        }
+    }
+}
