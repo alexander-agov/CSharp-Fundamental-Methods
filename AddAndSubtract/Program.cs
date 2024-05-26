74370db09a8cc1435b32ead3c5b13d0ffe609040
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 12:50:25 +0300

    Add files via upload

---
+++ AddAndSubtract/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int sum = SumOfFirstTwoInt(num1, num2);
+            int result = SubtractTheThirdInt(sum, num3);
+            Console.WriteLine(result);
+        }
+        static int SumOfFirstTwoInt(int a, int b)
+        {
+            return a + b;
+        }
+        static int SubtractTheThirdInt(int sum, int c)
+        {
+            return sum - c;
+        }
+    }
+}
