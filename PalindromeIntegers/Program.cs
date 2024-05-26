080ab44b6ec6121ddbd21be49513601d53e542a5
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 13:18:47 +0300

    Add files via upload

---
+++ PalindromeIntegers/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Globalization;
+
+namespace PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                Console.WriteLine(isNumberPalindrome(input));
+            }
+        }
+        static bool isNumberPalindrome(string input)
+        {
+            string firstHalf = input.Substring(0, input.Length / 2);
+            char[] charArr = input.ToCharArray();
+            Array.Reverse(charArr);
+            string temp = new string(charArr);
+            string lastHalf = temp.Substring(0, temp.Length / 2);
+            return firstHalf == lastHalf;
+        }
+    }
+}
+
