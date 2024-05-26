446dda39d87a6c7128a3d82847e72ba9d865ce79
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 13:13:58 +0300

    Add files via upload

---
+++ MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace MultiplyEvensByOdds
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            long number;
+            long.TryParse(input, out number);
+            long result = GetMultipleOfEvenAndOdds(number);
+            Console.WriteLine(result);
+        }
+        static long GetMultipleOfEvenAndOdds(long number)
+        {
+            long evenSum = GetSumOfEvenDigits(number);
+            long oddSum = GetSumOfOddDigits(number);
+            return evenSum * oddSum;
+        }
+        static long GetSumOfEvenDigits(long number)
+        {
+            number = Math.Abs(number);
+            long sum = 0;
+            while (number > 0)
+            {
+                int digit = (int)(number % 10);
+                if (digit % 2 == 0)
+                {
+                    sum += digit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+        static long GetSumOfOddDigits(long number)
+        {
+            number = Math.Abs(number);
+            long sum = 0;
+            while (number > 0)
+            {
+                int digit = (int)(number % 10);
+                if (digit % 2 != 0)
+                {
+                    sum += digit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
+
