c7977c06674cf87fc31b638b82849961dc1cbc72
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 13:40:37 +0300

    Add files via upload

---
+++ VowelsCount/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int result = CountVowels(word);
+            Console.WriteLine(result);
+        }
+        static int CountVowels(string word)
+        {
+            int count = 0;
+            string vowels = "aeiouAEIOU";
+            foreach (char c in word)
+            {
+                if (vowels.Contains(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
+
