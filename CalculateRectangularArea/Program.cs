a0023ac2010cdb048efa6c1b0ec7e072c4c50788
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 12:51:47 +0300

    Add files via upload

---
+++ CalculateRectangularArea/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CalculateRectangularArea
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = RectangularArea(width, height);
+            Console.WriteLine(area);
+        }
+
+        static double RectangularArea(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
