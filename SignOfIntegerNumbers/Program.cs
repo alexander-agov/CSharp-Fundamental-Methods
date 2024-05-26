a7df36cf616393349f8534ab307bc468227b54d1
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 13:28:23 +0300

    Add files via upload

---
+++ SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintNumberSign(n);
+            static void PrintNumberSign(int number)
+            {
+                if (number == 0)
+                {
+                    Console.WriteLine($"The number {number} is zero. ");
+                }
+                else if (number > 0)
+                {
+                    Console.WriteLine($"The number {number} is positive.");
+                }
+                else
+                {
+                    Console.WriteLine($"The number {number} is negative. ");
+                }
+            }
+        }
+    }
+}
---
+++ SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int result = SmallestNumber(num1, num2, num3);
+            Console.WriteLine(result);
+        }
+        static int SmallestNumber(int num1, int num2, int num3)
+        {
+            int result = 0;
+            if (num1 < num2 && num1 < num3)
+            {
+                result = num1;
+            }
+            else if (num2 < num1 && num2 < num3)
+            {
+                result = num2;
+            }
+            else
+            {
+                result = num3;
+            }
+            return result;
+        }
+    }
+}
