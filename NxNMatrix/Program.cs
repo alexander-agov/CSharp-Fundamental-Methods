2381dfa073f2be26e318d1b785a581beed7390e9
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 13:17:03 +0300

    Add files via upload

---
+++ NxNMatrix/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace NxNMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Column(num);
+        }
+        static void Column(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
