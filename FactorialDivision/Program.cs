fc3f986483f89e0dbd9a4c8e65167c5867aac60b
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 13:00:14 +0300

    Add files via upload

---
+++ FactorialDivision/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long num1 = long.Parse(Console.ReadLine());
+            long num2 = long.Parse(Console.ReadLine());
+            Console.WriteLine($"{(Factorial(num1) / Factorial(num2)):f2}");
+        }
+        static double Factorial(long num)
+        {
+            double result = 1;
+            for (long i = 1; i <= num; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
