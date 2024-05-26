970572d70aedfecce935eccaa1ef6eadec0ba4d2
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 13:08:44 +0300

    Add files via upload

---
+++ MiddleCharacters/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            if (word.Length % 2 == 0)
+            {
+                EvenChar(word);
+            }
+            else
+            {
+                OddChar(word);
+            }
+        }
+        static void OddChar(string word)
+        {
+            if (word.Length % 2 != 0)
+            {
+                Console.WriteLine(word[word.Length / 2]);
+            }
+        }
+        static void EvenChar(string word)
+        {
+            if (word.Length % 2 == 0)
+            {
+                Console.Write(word[word.Length / 2 - 1]);
+                Console.WriteLine(word[word.Length / 2]);
+            }
+        }
+    }
+}
