deaf7a5cd35692c86ab6e71d8a4bbbb8256ade76
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 13:24:20 +0300

    Add files via upload

---
+++ PrintingTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTriangle(n);
+        }
+        private static void PrintTriangle(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                PrintLine(row);
+            }
+            for (int row = n - 1; row >= 0; row--)
+            {
+                PrintLine(row);
+            }
+        }
+        static void PrintLine(int row)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write($"{col} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ RepeatedString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace RepeatedString
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string inputStr = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            string result = RepeatString(inputStr, count);
+            Console.WriteLine(result);
+        }
+        static string RepeatString(string str, int count)
+        {
+            StringBuilder result = new StringBuilder(); 
+            for (int i = 0; i < count; i++)
+                result.Append(str);
+            return result.ToString();                                 
+        }
+    }
+}
