702f56d2a1b9641e04a310737ef1b642108364e5
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-26 13:37:46 +0300

    Add files via upload

---
+++ TopNumber/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace TopNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int n = 1; n <= num; n++)
+            {
+                if (IsTopNumber(n))
+                {
+                    Console.WriteLine(n);
+                }
+            }
+        }
+        static bool IsTopNumber(int num)
+        {
+            if (SumOfDigitsDivisibleBy8(num) && HasAtLeastOneOddDigit(num))
+            {
+                return true;
+            }
+            return false;
+        }
+        static bool SumOfDigitsDivisibleBy8(int num)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum % 8 == 0;
+        }
+        static bool HasAtLeastOneOddDigit(int num)
+        {
+            while (num > 0)
+            {
+                int digit = num % 10;
+                if (digit % 2 != 0)
+                {
+                    return true;
+                }
+                num /= 10;
+            }
+            return false;
+        }
+    }
+}
---
+++ TribonacciSequence/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace TribonacciSequence
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            PrintTribonacciSequence(num);
+        }
+        static void PrintTribonacciSequence(int num)
+        {
+            if (num == 1 || num == 0)
+            {
+                Console.WriteLine(1);
+                return;
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("1 1");
+            }
+            else
+            {
+                int[] tribonacci = new int[num];
+                tribonacci[0] = 1;
+                tribonacci[1] = 1;
+                tribonacci[2] = 2;
+                Console.Write("1 1 2");
+                for (int i = 3; i < num; i++)
+                {
+                    tribonacci[i] = tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i - 3];
+                    Console.Write(" " + tribonacci[i]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
